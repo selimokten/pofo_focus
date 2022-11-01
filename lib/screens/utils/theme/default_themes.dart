9d99729b94028eac31dbb48ac2e3e3c6e20d68b2
Selim Okten <selimoktenofficial313@gmail.com>
2022-11-01 20:56:50 +0300

    Files Upload

---
+++ lib/screens/utils/theme/default_themes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DefaultTheme {
+  static ThemeData defaultThemeData(){
+    return ThemeData(
+      primaryColor: const Color(0xFF1F1B2E),
+    );
+  }
+}
