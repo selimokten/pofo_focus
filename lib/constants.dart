3f83d2d5a0856594a422caaf6e0b26ed355989e9
Selim Okten <selimoktenofficial313@gmail.com>
2022-11-05 21:24:22 +0300

    Files Upload

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const Color kBackGroundColor = Color(0xFF1E1B2E);
+const Color kSecondColor = Color(0xFF674FFF);
+
 const TextStyle kAppBarTextStyle = TextStyle(
   fontSize: 25.0,
   color: Colors.white,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,15 +10,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pomodoro timer', style: kAppBarTextStyle,),
+        title: const Text(
+          'Pomodoro timer',
+          style: kAppBarTextStyle,
+        ),
         centerTitle: true,
-        backgroundColor: DefaultThemes.defaultThemeData().scaffoldBackgroundColor,
+        backgroundColor:
+            DefaultThemes.defaultThemeData().scaffoldBackgroundColor,
         elevation: 0,
+      ),
+      body: SizedBox.expand(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 100.0,),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: kSecondColor,
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text('Write Task', style: kAppBarTextStyle,),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
