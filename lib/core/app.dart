828102831bc3a8ee223ffaa7615a54006cf15dfc
Selim Okten <selimoktenofficial313@gmail.com>
2022-11-10 19:22:31 +0300

    Files Upload

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,8 +1,7 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:po_focus/routes/app_routes.dart';
-
 import '../themes/default_theme.dart';
+
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -10,10 +9,11 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: DefaultThemes.defaultThemeData(),
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      initialRoute: "/homeView",
+      // locale: DevicePreview.locale(context),
+      // builder: DevicePreview.appBuilder,
+      initialRoute: "/",
       routes: AppRoutes.router,
     );
   }
